f6637f9dcc7233749cace04a41da782ccaa35c6b
Andre Cardoso <andrecardosodev@gmail.com>
2014-02-27 15:29:02 -0300

    Criada configuração inicial

---
+++ doctrine-na-pratica-leitura-livro/bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+$isDevMode = true;
+
+$paths = array(__DIR__ . '/src/DNP/Entity');
+
+$dbParams = array(
+    'driver' => 'pdo_mysql',
+    'user' => 'root',
+    'password' => 'root',
+    'dbname' => 'dnp',
+);
+
+$config = Setup::createConfiguration($isDevMode);
+
+$driver = new AnnotationDriver(new AnnotationReader(), $paths);
+$config->setMetadataDriverImpl($driver);
+
+AnnotationRegistry::registerFile(__DIR__ . '/vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php');
+
+$entityManager = EntityManager::create($dbParams, $config);
